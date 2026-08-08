aada357a3e1558d90e134a7db40284f2e937b5b5
Farhad-deve <Farkhad0527@gmail.com>
2026-08-08 16:03:01 +0500

    Files changed 16:07

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,5 +1,6 @@
+import { FiRotateCcw } from "react-icons/fi"; 
 import { FiCheck } from "react-icons/fi"; 
-import { ActionIcon, Button, Card, Code, ColorSwatch, Grid, Group, SegmentedControl, Stack, Text, ThemeIcon, Title, Tooltip, UnstyledButton } from "@mantine/core"
+import { ActionIcon, Box, Button, Card, Code, ColorSwatch, Grid, Group, SegmentedControl, Stack, Text, ThemeIcon, Title, Tooltip, UnstyledButton } from "@mantine/core"
 import { useState } from "react";
 
 const Settings = () => {
@@ -24,43 +25,72 @@ const Settings = () => {
           </Stack>
           
           <Grid>
-            <Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6 }}>
               <Stack>
-                <Card>
-                  <Text>1. Rangli rejim</Text>
-                  <Text>
-                    <Code>useMantineColorScheme()</Code>{" "}
-                    hook'i orqali boshqariladi
-                  </Text>
+                <Card withBorder>
+                  <Stack gap={"sm"}>
+                    <Box>
+                      <Text fw={500}>1. Rangli rejim</Text>
+                      <Text c={"dimmed"} size="sm">
+                        <Code>useMantineColorScheme()</Code>{" "}
+                        hook'i orqali boshqariladi
+                      </Text>
+                    </Box>
+
                   <SegmentedControl data={[
                     { label: "Yorug'", value: "light" },
                     { label: "Qorong'i", value: "dark" },
                     { label: "Avtomatik", value: "auto" },
                   ]} />
+                  </Stack>
                 </Card>
 
                 <Card>
-                  <Text>2. Asosiy rang</Text>
-                  <Text>Theme'dagi <Code>primaryColor</Code> maydoni</Text>
+                  <Stack gap={"sm"}>
+                    <Box>
+                      <Text fw={500}>2. Asosiy rang</Text>
+                      <Text c={"dimmed"} size="sm">Theme'dagi <Code>primaryColor</Code> maydoni</Text>
+                    </Box>
 
-                  <Group>
-                    {colorSwatchData.map((color) => (
-                      <Tooltip key={color.color} label={color.color}>
-                        <ColorSwatch
-                          component="button"
-                          onClick={() => setSelectedColor(color.color)} 
-                          color={color.value}
-                          style={{ cursor: "pointer" }}>
-                            {selectedColor === color.color && <FiCheck size={20} />}
-                        </ColorSwatch>
-                      </Tooltip>
-                    ))}
-                  </Group>
+                    <Group>
+                      {colorSwatchData.map((color) => (
+                        <Tooltip key={color.color} label={color.color}>
+                          <ColorSwatch
+                            component="button"
+                            onClick={() => setSelectedColor(color.color)} 
+                            color={color.value}
+                            style={{ cursor: "pointer" }}>
+                              {selectedColor === color.color && <FiCheck size={20} />}
+                          </ColorSwatch>
+                        </Tooltip>
+                      ))}
+                    </Group>
+
+                    <Text c={"dimmed"}>"brand" — mantine-theme.ts da o'zimiz qo'shgan rang</Text>
+                  </Stack>
                 </Card>
+
+                <Card>
+                  <Text>3. Burchaklar</Text>
+                  <Text>Theme'dagi <Code>defaultRadius</Code> maydoni </Text>
+                  <SegmentedControl
+                    data={[
+                      { label: "XS", value: "xs" },
+                      { label: "SM", value: "sm" },
+                      { label: "MD", value: "md" },
+                      { label: "LG", value: "lg" },
+                      { label: "XL", value: "xl" },
+                    ]}
+                  />
+                </Card>
+
+                <Button variant="default" leftSection={<FiRotateCcw />}>
+                  Standart holatga qaytarish
+                </Button>
               </Stack>
             </Grid.Col>
 
-            <Grid.Col>
+            <Grid.Col span={{ base: 12, md: 6 }}>
             
             </Grid.Col>
           </Grid>
